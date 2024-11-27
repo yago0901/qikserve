fd73a7af3d11f56f1cd90bce09d6f6fd1c532f57
Yago Santos Gigeck <yago0901@gmail.com>
2024-11-27 00:46:41 -0300

    feat: created input search

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,13 +1,14 @@
+import lupa from "../../assets/lupa.png"
 import "./styles.scss";
 
 export const Body = () => {
   return (
     <div className='body'>
       <div className='body__search_container'>
-        <input className='body__search_container__search' type="text" placeholder='Search menu items' />
-        <div className='body__search_container__background'>
-
-        </div>
+        <input className='body__search_container__search' type="text" placeholder='Search menu itens' />
+        <img className='body__search_container__search_icon' src={lupa} alt="" />
+      </div> 
+      <div className='body__search_container__background'>
       </div>
     </div>
   )
