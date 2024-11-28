03b80cc495a6bc7d07846b104c096c4efffe992b
Yago Santos Gigeck <yago0901@gmail.com>
2024-11-28 01:51:24 -0300

    feat: added menu and cart structure

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,14 +1,28 @@
-import lupa from "../../assets/lupa.png"
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./styles.scss";
 
 export const Body = () => {
   return (
     <div className='body'>
       <div className='body__search_container'>
-        <img className='body__search_container__search_icon' src={lupa} alt="" />
+        <div className='body__search_container__icon_container'>
+          <FontAwesomeIcon className='body__search_container__icon_container__icon' icon={faSearch} />
+        </div>
         <input className='body__search_container__search' type="text" placeholder='Search menu itens' />
-      </div> 
+      </div>
       <div className='body__search_container__background'>
+        <div className='body__search_container__background__items'>
+          teste
+        </div>
+        <div className='body__search_container__background__cart'>
+          <div className='body__search_container__background__cart__title'>
+            Carrinho
+          </div>
+          <div className='body__search_container__background__cart__content'>
+            Seu carrinho está vazio
+          </div>
+        </div>
       </div>
     </div>
   )
